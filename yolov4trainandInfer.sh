e08ac8913005792c37892eee7271a256b9ed1885
bUbuntu <braveendra@outlook.com>
2021-10-12 08:43:34 -0400

    update yolov4.sh

--- yolov4trainandInfer.sh
+++ yolov4trainandInfer.sh
@@ -4,7 +4,12 @@
 cd yolov4/
 mkdir maskbackup
 mkdir output
-wget "https://drive.google.com/open?id=1JKF-bdIklxOOVy-2Cr5qdvjgGpmGfcbp" -O yolov4.conv.137
+
+fileid="1JKF-bdIklxOOVy-2Cr5qdvjgGpmGfcbp"
+filename="yolov4.conv.137"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+#wget "https://drive.google.com/u/0/open?id=1JKF-bdIklxOOVy-2Cr5qdvjgGpmGfcbp" -O yolov4.conv.137
 ./../darknet/darknet detector train yolov4-mask-setup.data yolov4-obj.cfg ./yolov4.conv.137 -dont_show -map 2> train_log.txt
 ./test.sh
 
